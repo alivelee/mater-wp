e06605c03d6c07cbc0100cdfd54e8e593c565b5a
alivelee <a871460434@gmail.com>
2016-04-12 22:40:47 +0800

    add testing grid and remove redundantly style

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+var sass        = require('gulp-sass');
+gulp.task('browser-sync', function() {
+    //watch files
+    var files = [
+    './style.css',
+    './*.php'
+    ];
+
+    //initialize browsersync
+    browserSync.init(files, {
+    //browsersync with a php server
+    proxy: "localhost:8888",
+    notify: false
+    });
+});
+
+// Sass task, will run when any SCSS files change & BrowserSync
+// will auto-update browsers
+gulp.task('sass', function () {
+    return gulp.src('sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./'))
+        .pipe(reload({stream:true}));
+});
+
+// Default task to be run with `gulp`
+gulp.task('default', ['sass', 'browser-sync'], function () {
+    gulp.watch("sass/**/*.scss", ['sass']);
+});
